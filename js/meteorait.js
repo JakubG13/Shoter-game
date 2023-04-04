516c28851ed3fbe3f08348f1c9ba974cb7b24b28
Jakub Gaj <gaju2001@o2.pl>
2023-04-04 17:02:10 +0200

    meteorites param change

--- js/meteorait.js
+++ js/meteorait.js
@@ -2,14 +2,14 @@ let metNumber = 1;
 class Met {
   constructor() {}
   createMet() {
-    let metWidthHeight = Math.floor(Math.random() * 100 + 50);
+    let metWidthHeight = Math.floor(Math.random() * 75 + 75);
     let metLeft = Math.floor(Math.random() * 80 + 10);
-    let metDuration = Math.floor(Math.random() * 3 + 1);
+    let metDuration = Math.floor(Math.random() * 2 + 4);
 
     const met = document.createElement("div");
     met.classList.add(`met${metNumber}`);
     metNumber++;
-    if (metNumber === 4) {
+    if (metNumber === 6) {
       metNumber = 1;
     }
     met.style.width = metWidthHeight + "px";
@@ -19,7 +19,7 @@ class Met {
     document.body.appendChild(met);
     setTimeout(() => {
       document.body.removeChild(met);
-    }, 3000);
+    }, 5000);
   }
 }
 
